3648fee3c93d25bc1abb75f670c24f8ec8deaeac
Miles Debinski <miles.debinski@gmail.com>
2021-05-11 14:49:40 +0100

    add smooth transitions

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,30 @@ exit.addEventListener("click", () => {
   overlay.classList.toggle("active");
   mobile.classList.toggle("active");
 });
+
+// viewport items
+const mainItems = document.querySelectorAll("#appear_itm");
+
+const mainIsInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= -200 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) + 50 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const mainRun = () => {
+  mainItems.forEach((item) => {
+    if (mainIsInViewport(item)) {
+      item.classList.add("main_show");
+    }
+  });
+};
+
+//Trigger Events
+window.addEventListener("load", mainRun);
+window.addEventListener("resize", mainRun);
+window.addEventListener("scroll", mainRun);
