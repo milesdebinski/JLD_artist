e3810ee8c9ffdaa4de3880e07eac4ed40b7b924f
Miles Debinski <miles.debinski@gmail.com>
2021-06-17 23:52:20 +0100

    add footer links

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,8 @@ exit.addEventListener("click", () => {
 
 // viewport items
 const mainItems = document.querySelectorAll("#appear_itm");
-console.log(window.innerWidth);
+const mainItemsMobile = document.querySelectorAll("#appear_itm_mobile");
+
 const mainIsInViewport = (el) => {
   const rect = el.getBoundingClientRect();
 
@@ -31,10 +32,28 @@ const mainIsInViewport = (el) => {
       (window.innerHeight || document.documentElement.clientHeight) + 60
   );
 };
-
 const mainRun = () => {
   mainItems.forEach((item) => {
     if (mainIsInViewport(item)) {
+      setTimeout(() => {
+        item.classList.add("main_show");
+      }, 250);
+    }
+  });
+};
+
+const main_moileIsInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= -200 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) + 340
+  );
+};
+const mainRun_mobile = () => {
+  mainItemsMobile.forEach((item) => {
+    if (main_moileIsInViewport(item)) {
       setTimeout(() => {
         item.classList.add("main_show");
       }, 250);
@@ -47,6 +66,9 @@ const mainRun = () => {
 window.addEventListener("load", mainRun);
 window.addEventListener("resize", mainRun);
 window.addEventListener("scroll", mainRun);
+window.addEventListener("load", mainRun_mobile);
+window.addEventListener("resize", mainRun_mobile);
+window.addEventListener("scroll", mainRun_mobile);
 
 // Onscroll Navbar
 const navbar = document.querySelectorAll("#navbar");
@@ -78,4 +100,18 @@ window.addEventListener("mousemove", (el) => {
       item.classList.remove("fixed");
     });
   }
+});
+// logo link, call number on click, open mailer
+const logo_home = document.getElementById("logo_home");
+const phone_number = document.getElementById("phone_number");
+const email_address = document.getElementById("email_address");
+
+logo_home.addEventListener("click", () => {
+  window.open("../index.html", "_self");
+});
+phone_number.addEventListener("click", () => {
+  window.open("tel:+447391610097");
+});
+email_address.addEventListener("click", () => {
+  window.open("mailto:janlesz@gmx.net");
 });
